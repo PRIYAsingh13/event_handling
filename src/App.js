5a330517010cbcb7cba661be3d19030885bb7368
PRIYAsingh13 <ps597934@gmail.com>
2023-02-03 23:19:17 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const purple = "pink";
+
+  const [bg, setBg] = useState(purple);
+  const [name, setName] = useState('click me')
+
+  const bgchange = () => {
+    let newBg = "yellow";
+    setBg(newBg)
+    setName("ohhh!")
+  };
+  const bgBack = () =>{
+    setBg(purple)
+    setName("💁‍♀️")
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ backgroundColor: bg }}>
+        <button onMouseEnter={bgchange} onMouseLeave={bgBack}> {name}</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
